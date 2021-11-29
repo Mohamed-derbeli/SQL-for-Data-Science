dba53c69f61aa614eeb1db337f3a82bebf162955
Mohamed Derbeli <86875309+Mohamed-derbeli@users.noreply.github.com>
2021-11-30 00:30:54 +0100

    Add files via upload

---
+++ Exercise 3 TRUNCATE and DROP.sql
@@ -0,0 +1,12 @@
+
+--  TRUNCATE  (Remove all rows from the PET table and show the empty table.) 
+TRUNCATE TABLE PET IMMEDIATE;
+
+SELECT * FROM PET;
+
+
+--  DROP (Delete the PET table )
+
+DROP TABLE PET;
+
+SELECT * FROM PET;
