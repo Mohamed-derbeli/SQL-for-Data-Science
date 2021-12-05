c6190dd4e3625e7ec3bcb74d4dd5e5fd0458602f
Mohamed Derbeli <86875309+Mohamed-derbeli@users.noreply.github.com>
2021-12-05 22:46:56 +0100

    Update String Patterns_Sorting_Grouping_range_count.sql

--- String Patterns_Sorting_Grouping_range_count.sql
+++ String Patterns_Sorting_Grouping_range_count.sql
@@ -1,8 +1,10 @@
 --Retrieving rows using string pattern
 --Query 1: Retrieve all employees whose address is in Elgin,IL
-SELECT F_NAME, L_NAME FROM EMPLOYEES WHERE ADDRESS LIKE '%Elgin,IL%'
+SELECT F_NAME, L_NAME FROM EMPLOYEES 
+WHERE ADDRESS LIKE '%Elgin,IL%'
 --Query 2: Retrieve all employees who were born during the 1970's.
-SELECT F_NAME, L_NAME FROM EMPLOYEES WHERE B_DATE LIKE '197%'
+SELECT F_NAME, L_NAME FROM EMPLOYEES 
+WHERE B_DATE LIKE '197%'
 
 --Retrieving rows using Ranges
 --Query 3: Retrieve all employees in department 5 whose salary is between 60000 and 70000 .
